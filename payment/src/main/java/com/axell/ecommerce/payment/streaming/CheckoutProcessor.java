acbbfed67153a4c481d27c8f7c7fa04739b9f225
axell-brendow <breno.axel@gmail.com>
2021-03-01 15:41:17 -0300

    Configure Cloud Stream I/O channels

---
+++ payment/src/main/java/com/axell/ecommerce/payment/streaming/CheckoutProcessor.java
@@ -0,0 +1,17 @@
+package com.axell.ecommerce.payment.streaming;
+
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+
+public interface CheckoutProcessor {
+    String OUTPUT = "payment-paid-output";
+    String INPUT = "checkout-created-input";
+
+    @Output(OUTPUT)
+    MessageChannel output();
+
+    @Input(INPUT)
+    SubscribableChannel input();
+}
